5fefa3536aff697cb25dfff4fa732c1160a88b16
anthony peter <www.anthonygoldpeter@gmail.com>
2025-04-28 04:13:54 -0700

    push

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,7 @@ let baseUrl;
 if (process.env.NODE_ENV === "development") {
     baseUrl = "http://localhost:5000";
 } else {
-    baseUrl = "https://nodesockect.onrender.com/";
+    baseUrl = "https://nodesockect.onrender.com";
 }
 
 export default baseUrl;
